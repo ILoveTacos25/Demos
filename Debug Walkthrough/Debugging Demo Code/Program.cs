e74a9499791c9e71f0bc618479d63d27fe922a3b
Adam <adam.wagener@wsu.edu>
2023-04-27 19:23:36 -0700

    presentation and my demo code added

---
+++ Debug Walkthrough/Debugging Demo Code/Program.cs
@@ -0,0 +1,14 @@
+﻿using Debugging_Demo_Code;
+
+public class Program
+{
+    public static void Main()
+    {
+        int a, b, c, d;
+
+        for (int i = 1; i < 1000; i++)
+        {
+            Console.WriteLine(VerdiantComputer.ComputeVerdiance(i));
+        }
+    }
+}
---
+++ Debug Walkthrough/Debugging Demo Code/VerdiantComputer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging_Demo_Code
+{
+    public class VerdiantComputer
+    {
+        public static bool ComputeVerdiance(int a)
+        {
+            int b = 0;
+            int c = 0;
+            int d = a;
+
+            for (int i = 0; i < a; i++)
+            {
+                c = a / 3;
+                b = ((i+1) * c) / a;
+                if (isHalfVerdiant(b, c))
+                {
+                    d -= 10;
+                    if (d <= 0)
+                    {
+                        d = a;
+                    }
+                }
+            }
+
+            if (d % a == b % a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool isHalfVerdiant(int b, int c)
+        {
+            for (int j = 1; j <= c; j=j*2)
+            {
+                if (c % j == b * j)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
